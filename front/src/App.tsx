8ab5865c92c7cd0296ef979173273f22ec0105ae
Ari Lotter <arilotter@gmail.com>
2022-10-04 23:15:33 -0400

    credits :)

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useState } from "react";
 import { Connect } from "./components/ConnectButton";
 import { ServerConnection } from "./components/ServerConnection";
 
 import skyweaverLogo from "./assets/sw.png";
 import vaportradeIcon from "./assets/vaportrade.png";
 
+import "./App.css";
+import { useState } from "react";
+import { Credits } from "./components/Credits";
+
 export function App() {
+  const [credits, setCredits] = useState(false);
   return (
     <>
-      <header className="title">
-        <img
-          src={vaportradeIcon}
-          style={{ paddingLeft: "8px", height: "60%" }}
-        />{" "}
+      <header className="title" onClick={() => setCredits(true)}>
+        <img src={vaportradeIcon} />
         &times;
-        <img
-          src={skyweaverLogo}
-          style={{ paddingLeft: "8px", height: "60%" }}
-        />
+        <img src={skyweaverLogo} />
       </header>
+      {credits && <Credits onClose={() => setCredits(false)} />}
       <Connect
         afterConnect={(wallet, address) => (
           <ServerConnection wallet={wallet} address={address} />
---
+++ front/src/components/Credits/index.tsx
@@ -0,0 +1,69 @@
+import "./Credits.css";
+
+interface CreditsProps {
+  onClose: () => void;
+}
+
+export function Credits({ onClose }: CreditsProps) {
+  return (
+    <div className="credits">
+      <button onClick={onClose}>&times;</button>
+      <div className="creditsContents">
+        <h1>Credits</h1>
+        <h2>Concept &amp; Design</h2>
+        <p>
+          <a href="https://ari.computer/">Ari Lotter</a> &amp;{" "}
+          <a href="https://www.instagram.com/matthewshera/">Matthew Shera</a>
+        </p>
+        <h2>Vaportrade Logo</h2>
+        <p>
+          <a href="https://www.instagram.com/soapy_scribbles/">
+            @soapy_scribbles
+          </a>
+        </p>
+        <h2>Development</h2>
+        <p>
+          Ari Lotter with heaps of{" "}
+          <a href="https://docs.swapsdk.xyz">nft-swap-sdk</a> help from @hazucf
+          of <a href="https:/trader.xyz">trader.xyz</a> and unending Sequence
+          guidance from William Hua, Michael Yu, Philippe Castonguay, &amp;
+          Agustin Aguilar.
+        </p>
+        <h2>Dependencies</h2>
+        <p>
+          <a href="https://docs.swapsdk.xyz">nft-swap-sdk</a>,{" "}
+          <a href="https://0x.org/">0x v3</a>,{" "}
+          <a href="https://sequence.xyz/">sequence.js</a>,{" "}
+          <a href="https://ethers.org/">ethers</a>,{" "}
+          <a href="https://github.com/stephensprinkle-zz/react-blockies">
+            react blockies
+          </a>
+          <a href="https://github.com/robtaussig/react-use-websocket">
+            react-use-websocket
+          </a>
+          , &amp; <a href="https://reactjs.org/">react</a>
+        </p>
+        <p>
+          Vaportrade &times; Skyweaver is dedicated to everyone in the Skyweaver
+          Discord who got scammed - you motivated me to never let that happen
+          again.
+        </p>
+        <p>
+          Follow
+          <a href="https://twitter.com/usevaportrade"> @usevaportrade </a> on
+          that bird app or shoot me an email at{" "}
+          <a href="mailto:vaportrade.net@gmail.com">vaportrade.net@gmail.com</a>
+        </p>
+        <p>
+          skyweaver.vaportrade.net is open source!{" "}
+          <a href="https://github.com/arilotter/skyweaver.vaportrade/">
+            https://github.com/arilotter/skyweaver.vaportrade/
+          </a>
+        </p>
+        <p>
+          Try the original <a href="https://vaportrade.net">vaportrade.net</a>
+        </p>
+      </div>
+    </div>
+  );
+}
